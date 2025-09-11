dab5acb1132cf790fc629b025229b731b0b7710e
Susanagc <susigc0304@gmail.com>
2025-09-11 06:54:19 -0500

    add fizzbuzz

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,31 @@
 export default function Home() {
+
+  function FizzBuzz(): void {
+    for (let x: number = 1; x <= 50; x++) {
+      if (x % 3 === 0 && x % 5 === 0) {
+        console.log("FizzBuzz");
+      } else if (x % 3 === 0) {
+        console.log("Fizz");
+      } else if (x % 5 === 0) {
+        console.log("Buzz");
+      } else {
+        console.log(x);
+      }
+    }
+  }
+  
+  FizzBuzz();
+  
   return (
     <main className="main-container">
       <h1>HELLO WORLD!! ✨</h1>
       <h2>Mi primer ¡Hola Mundo! desde Next.js + TypeScript! 🤍</h2>
+    
+    
     </main>
+
+
+
   );
 }
+
